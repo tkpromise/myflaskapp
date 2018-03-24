0d1ec428c443e340489ab6f6a3de3e0e311cbbaf
tkpromise <tkpromise@foxmail.com>
2018-03-25 02:34:07 +0800

    first

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Aritcle One',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor in',
+			'author': 'Brad Traversy',
+			'create_data': '04-25-2017'
+		},
+		{
+			'id': 2,
+			'title': 'Article Two',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor in',
+			'author': 'John Doe',
+			'create_data': '04-25-2017'
+		},
+		{
+			'id': 3,
+			'title': 'Article Three',
+			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor in',
+			'author': 'John Doe',
+			'create_data': '04-25-2017'
+		}
+	]
+	return articles
